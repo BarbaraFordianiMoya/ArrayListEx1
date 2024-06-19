c441ab585e5d19bd5931ef76cbe73771a1bc9839
Aluno <Aluno@10.85.198.10>
2024-06-19 15:50:14 -0300

    ArrayListEx1

---
+++ src/ArrayListEx1.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class ArrayListEx1 {
+
+    public static void main(String[] args) {
+        ArrayList<String> estudantes;
+        estudantes = new ArrayList();
+        estudantes.add("Nick");
+        estudantes.add("Mike");
+        estudantes.add("Amy");
+        estudantes.add("Cindy");
+        estudantes.add("David");
+        System.out.println(estudantes);
+    }
+    
+}
